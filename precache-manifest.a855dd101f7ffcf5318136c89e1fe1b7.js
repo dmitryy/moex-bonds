0ad1bea2a2af71512a68fdba1853711ccf126766
dmitryy <dmitry@ruswizards.com>
2020-05-27 00:23:56 +0300

    Updates

---
+++ precache-manifest.a855dd101f7ffcf5318136c89e1fe1b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447df124b690077d939157c7f300afb5",
+    "url": "/moex-bonds/index.html"
+  },
+  {
+    "revision": "c003868e63fe562346c9",
+    "url": "/moex-bonds/static/css/main.c20bb07b.chunk.css"
+  },
+  {
+    "revision": "cc32abeed899907e9731",
+    "url": "/moex-bonds/static/js/2.6fcaa3d3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/moex-bonds/static/js/2.6fcaa3d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c003868e63fe562346c9",
+    "url": "/moex-bonds/static/js/main.5e0f2623.chunk.js"
+  },
+  {
+    "revision": "7ffba69abc9cbf6b0c6e",
+    "url": "/moex-bonds/static/js/runtime-main.15102b68.js"
+  }
+]);
